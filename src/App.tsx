cf293988c2afb2791de8b668ca5f5c68794a6b4e
hemdee <sanusi5110@gmail.com>
2023-02-18 18:34:47 +0100

    update path

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { Routes, Route } from "react-router-dom";
-import { Home } from "./pages";
-import TestingAPI from "./pages/TestingAPI";
+import { Home, TestingAPI } from "./pages";
 
 function App() {
   return (

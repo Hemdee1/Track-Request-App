89e69beeb27b85e7366c11287ea2ae6a3e01eec0
hemdee <sanusi5110@gmail.com>
2023-02-18 18:14:37 +0100

    updated pacakge

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import { Home } from "./pages";
+import TestingAPI from "./pages/TestingAPI";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/test" element={<TestingAPI />} />
     </Routes>
   );
 }

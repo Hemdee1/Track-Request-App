b16e6351483fa2804f75c6b17f01d8b41c2aad4e
hemdee <sanusi5110@gmail.com>
2023-02-19 01:14:16 +0100

    set up cloudinary API

---
+++ src/hooks/useUpload.ts
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+export const uploadImage = async (file: File) => {
+  const formData = new FormData();
+  formData.append("file", file);
+  formData.append("upload_preset", "mxrequest");
+
+  try {
+    const res = await axios.post(
+      `https://api.cloudinary.com/v1_1/${process.env.CLOUD_NAME}/image/upload`,
+      formData
+    );
+
+    return res.data.secure_url;
+  } catch (error) {
+    console.log(error);
+  }
+};
